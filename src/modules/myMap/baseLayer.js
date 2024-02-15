69a8f1955f3ebb09de9252020dc987039472f6d4
Julian Bogdani <jbogdani@gmail.com>
2024-02-15 13:53:23 +0100

    Added corona images

--- src/modules/myMap/baseLayer.js
+++ src/modules/myMap/baseLayer.js
@@ -1,21 +1,17 @@
 import React from "react"
-import { TileLayer,LayersControl } from "react-leaflet"
+import { TileLayer, LayersControl } from "react-leaflet"
 
-const BaseLayer = ({
-  name,
-  checked,
-  url,
-  attribution
-}) => {
+const BaseLayer = ({ name, checked, url, attribution, asOverlay }) => {
+  if (asOverlay) {
+    return (
+      <LayersControl.Overlay checked={checked} name={name}>
+        <TileLayer url={url} attribution={attribution ? attribution : false} />
+      </LayersControl.Overlay>
+    )
+  }
   return (
-    <LayersControl.BaseLayer
-      checked={ checked }
-      name={ name }
-    >
-      <TileLayer
-        url={url}
-        attribution={attribution ? attribution : false}
-      />
+    <LayersControl.BaseLayer checked={checked} name={name}>
+      <TileLayer url={url} attribution={attribution ? attribution : false} />
     </LayersControl.BaseLayer>
   )
 }
