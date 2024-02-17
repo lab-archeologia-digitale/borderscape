e2045cb86bb113ff0e959f2dc85e0e4ae9a77800
Julian Bogdani <jbogdani@gmail.com>
2024-02-17 08:21:02 +0100

    Updated header

--- src/components/header.js
+++ src/components/header.js
@@ -2,42 +2,40 @@ import * as React from "react"
 import { Link, withPrefix } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import styled from "styled-components"
-import { Container } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
 
 const HeaderSection = ({ siteTitle }) => (
   <Header>
     <Container>
-      <div className="d-flex flex-row align-items-center mb-3">
-        <Link to={ withPrefix('/') }>
-          <StaticImage
-            src="../images/borderscape.svg"
-            width={250}
-            quality={80}
-            formats={["AUTO", "WEBP"]}
-            alt={siteTitle}
-            className="img-fluid"
-          />
-        </Link>
-        <div className="ms-3 lead">
-          WebGIS of Egyptian State Formation and the Changing Socio-Spatial Landscape of the Nile First Cataract Region in the 4th and 3rd millennia BCE
-        </div>
-      </div>
+      <Row className="text-center">
+        <Col xs={12} md={3}>
+          <Link to={withPrefix("/")}>
+            <StaticImage
+              src="../images/borderscape.svg"
+              width={250}
+              quality={80}
+              formats={["AUTO", "WEBP"]}
+              alt={siteTitle}
+              className="img-fluid"
+            />
+          </Link>
+        </Col>
+        <Col>
+          <div className="lead pt-5">
+            WebGIS of Egyptian State Formation and the Changing Socio-Spatial
+            Landscape of the Nile First Cataract Region in the 4th and 3rd
+            millennia BCE
+          </div>
+        </Col>
+      </Row>
     </Container>
   </Header>
 )
 
 const Header = styled.header`
-  background-color: #EE8822;
+  background-color: #ee8822;
   color: #ffffff;
   margin-bottom: 5rem;
-
-  .gatsby-image-wrapper {
-    background-color: #ffffff;
-    img {
-      padding-left: 1rem;
-      padding-right: 1rem;
-    }
-  }
 `
 
 export default HeaderSection
